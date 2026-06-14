687b011ff33309992ab9e4040be7a77ef1b538d0
Nickson4k <nickson4k@gmail.com>
2026-06-14 17:09:10 +0300

    fix: inject more STUN servers for better NAT traversal without TURN

--- src/stores/p2pStore.ts
+++ src/stores/p2pStore.ts
@@ -47,7 +47,7 @@ export const useP2PStore = create<P2PState>((set, get) => ({
       }, 20000);
 
       try {
-        const peerConfig = { config: { iceServers: [ { urls: "stun:stun.l.google.com:19302" }, { urls: "stun:global.stun.twilio.com:3478" }, { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" } ] } }; const customId = useAuthStore.getState().user?.peer_id;
+        const peerConfig = { config: { iceServers: [ { urls: "stun:stun.l.google.com:19302" }, { urls: "stun:stun1.l.google.com:19302" }, { urls: "stun:stun2.l.google.com:19302" }, { urls: "stun:stun3.l.google.com:19302" }, { urls: "stun:stun4.l.google.com:19302" }, { urls: "stun:global.stun.twilio.com:3478" }, { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" } ] } }; const customId = useAuthStore.getState().user?.peer_id;
           const peer = customId ? new Peer(customId, peerConfig) : new Peer(peerConfig);
 
         peer.on('open', (id) => {
@@ -101,7 +101,7 @@ export const useP2PStore = create<P2PState>((set, get) => ({
       }, 20000);
 
       try {
-        const peerConfig = { config: { iceServers: [ { urls: "stun:stun.l.google.com:19302" }, { urls: "stun:global.stun.twilio.com:3478" }, { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" } ] } }; const customId = useAuthStore.getState().user?.peer_id;
+        const peerConfig = { config: { iceServers: [ { urls: "stun:stun.l.google.com:19302" }, { urls: "stun:stun1.l.google.com:19302" }, { urls: "stun:stun2.l.google.com:19302" }, { urls: "stun:stun3.l.google.com:19302" }, { urls: "stun:stun4.l.google.com:19302" }, { urls: "stun:global.stun.twilio.com:3478" }, { urls: "turn:openrelay.metered.ca:80", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443", username: "openrelayproject", credential: "openrelayproject" }, { urls: "turn:openrelay.metered.ca:443?transport=tcp", username: "openrelayproject", credential: "openrelayproject" } ] } }; const customId = useAuthStore.getState().user?.peer_id;
           const peer = customId ? new Peer(customId, peerConfig) : new Peer(peerConfig);
 
         peer.on('open', (id) => {
